fbe326c237355d24beb7798728e1ba2f0bb4750b
Voja <Voja@DESKTOP-DCNG86L>
2017-08-04 10:56:10 +0200

    06 templates

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.send('working');
+    res.render('index',{
+        title: 'Home'   
+    });
 });
 
 // Start the server
