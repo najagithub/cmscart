5087efc27b4fab8b348e3a32fb17593373e82d7f
Voja <Voja@DESKTOP-DCNG86L>
2017-08-04 09:54:38 +0200

    05 connecting to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+
+// Connect to db
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to MongoDB');
+});
 
 // Init app
 var app = express();
