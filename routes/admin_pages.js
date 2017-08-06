1595c669e874c82d63e4de262fda2ed1b3a5ccc4
Voja <Voja@DESKTOP-DCNG86L>
2017-08-06 10:48:33 +0200

    12 post add page 2

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,43 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+// Get Page model
+var Page = require('../models/page');
+
 /*
  * GET pages index
  */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.send('admin area');
 });
 
 /*
  * GET add page
  */
-router.get('/add-page', function(req, res) {
-    
+router.get('/add-page', function (req, res) {
+
     var title = "";
     var slug = "";
     var content = "";
-    
+
     res.render('admin/add_page', {
         title: title,
         slug: slug,
         content: content
     });
-    
+
 });
 
 /*
  * POST add page
  */
-router.post('/add-page', function(req, res) {
-    
+router.post('/add-page', function (req, res) {
+
     req.checkBody('title', 'Title must have a value.').notEmpty();
     req.checkBody('content', 'Content must have a value.').notEmpty();
-    
+
     var title = req.body.title;
     var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
-    if (slug == "") slug = title.replace(/\s+/g, '-').toLowerCase();
+    if (slug == "")
+        slug = title.replace(/\s+/g, '-').toLowerCase();
     var content = req.body.content;
-    
+
     var errors = req.validationErrors();
 
     if (errors) {
@@ -48,9 +52,33 @@ router.post('/add-page', function(req, res) {
             content: content
         });
     } else {
-        console.log('success');
+        Page.findOne({slug: slug}, function (err, page) {
+            if (page) {
+                req.flash('danger', 'Page slug exists, choose another.');
+                res.render('admin/add_page', {
+                    title: title,
+                    slug: slug,
+                    content: content
+                });
+            } else {
+                var page = new Page({
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    sorting: 0
+                });
+
+                page.save(function (err) {
+                    if (err)
+                        return console.log(err);
+
+                    req.flash('success', 'Page added!');
+                    res.redirect('/admin/pages');
+                });
+            }
+        });
     }
-    
+
 });
 
 // Exports
