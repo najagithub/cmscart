6e2c47be0a66276b59000267a0740ce7acae38ba
Voja <Voja@DESKTOP-DCNG86L>
2017-08-06 10:08:36 +0200

    10 get add page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+ * GET pages index
+ */
 router.get('/', function(req, res) {
     res.send('admin area');
+});
+
+/*
+ * GET add page
+ */
+router.get('/add-page', function(req, res) {
+    
+    var title = "";
+    var slug = "";
+    var content = "";
+    
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    });
+    
 });
 
 // Exports
