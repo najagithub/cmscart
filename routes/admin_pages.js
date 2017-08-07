f71c388d2ab231ac539424809cd532a30f80a7b4
Voja <Voja@DESKTOP-DCNG86L>
2017-08-07 10:06:54 +0200

    15 get edit page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -19,35 +19,35 @@ router.get('/', function (req, res) {
  * GET add page
  */
 router.get('/add-page', function (req, res) {
-    
+
     var title = "";
     var slug = "";
     var content = "";
-    
+
     res.render('admin/add_page', {
         title: title,
         slug: slug,
         content: content
     });
-    
+
 });
 
 /*
  * POST add page
  */
 router.post('/add-page', function (req, res) {
-    
+
     req.checkBody('title', 'Title must have a value.').notEmpty();
     req.checkBody('content', 'Content must have a value.').notEmpty();
-    
+
     var title = req.body.title;
     var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
     if (slug == "")
         slug = title.replace(/\s+/g, '-').toLowerCase();
     var content = req.body.content;
-    
+
     var errors = req.validationErrors();
-    
+
     if (errors) {
         res.render('admin/add_page', {
             errors: errors,
@@ -71,18 +71,18 @@ router.post('/add-page', function (req, res) {
                     content: content,
                     sorting: 100
                 });
-                
+
                 page.save(function (err) {
                     if (err)
                         return console.log(err);
-                    
+
                     req.flash('success', 'Page added!');
                     res.redirect('/admin/pages');
                 });
             }
         });
     }
-    
+
 });
 
 /*
@@ -90,24 +90,45 @@ router.post('/add-page', function (req, res) {
  */
 router.post('/reorder-pages', function (req, res) {
     var ids = req.body['id[]'];
-    
+
     var count = 0;
-    
+
     for (var i = 0; i < ids.length; i++) {
         var id = ids[i];
         count++;
-        
-        (function(count) {
-        Page.findById(id, function (err, page) {
-            page.sorting = count;
-            page.save(function (err) {
-                if (err)
-                    return console.log(err); 
+
+        (function (count) {
+            Page.findById(id, function (err, page) {
+                page.sorting = count;
+                page.save(function (err) {
+                    if (err)
+                        return console.log(err);
+                });
             });
-        });
         })(count);
 
     }
+});
+
+/*
+ * GET edit page
+ */
+router.get('/edit-page/:slug', function (req, res) {
+
+    Page.findOne({slug: req.params.slug}, function (err, page) {
+        if (err)
+            return console.log(err);
+
+        res.render('admin/edit_page', {
+            title: page.title,
+            slug: page.slug,
+            content: page.content,
+            id: page._id
+        });
+    });
+
+
+
 });
 
 // Exports
