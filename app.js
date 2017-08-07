5778aa80d5c1237fb775f0f6a8b39dee0b67be65
Voja <Voja@DESKTOP-DCNG86L>
2017-08-07 16:46:25 +0200

    18 admin categories model and route setup

--- app.js
+++ app.js
@@ -70,8 +70,10 @@ app.use(function (req, res, next) {
 // Set routes 
 var pages = require('./routes/pages.js');
 var adminPages = require('./routes/admin_pages.js');
+var adminCategories = require('./routes/admin_categories.js');
 
 app.use('/admin/pages', adminPages);
+app.use('/admin/categories', adminCategories);
 app.use('/', pages);
 
 // Start the server
