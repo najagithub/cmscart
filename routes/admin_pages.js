9a988af8411770c83a129d36d21eb319b59edb4f
Voja <Voja@DESKTOP-DCNG86L>
2017-08-07 11:41:10 +0200

    16 post edit page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -127,7 +127,67 @@ router.get('/edit-page/:slug', function (req, res) {
         });
     });
 
+});
 
+/*
+ * POST edit page
+ */
+router.post('/edit-page/:slug', function (req, res) {
+
+    req.checkBody('title', 'Title must have a value.').notEmpty();
+    req.checkBody('content', 'Content must have a value.').notEmpty();
+
+    var title = req.body.title;
+    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+    if (slug == "")
+        slug = title.replace(/\s+/g, '-').toLowerCase();
+    var content = req.body.content;
+    var id = req.body.id;
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('admin/edit_page', {
+            errors: errors,
+            title: title,
+            slug: slug,
+            content: content,
+            id: id
+        });
+    } else {
+        Page.findOne({slug: slug, _id: {'$ne': id}}, function (err, page) {
+            if (page) {
+                req.flash('danger', 'Page slug exists, choose another.');
+                res.render('admin/edit_page', {
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    id: id
+                });
+            } else {
+
+                Page.findById(id, function (err, page) {
+                    if (err)
+                        return console.log(err);
+
+                    page.title = title;
+                    page.slug = slug;
+                    page.content = content;
+
+                    page.save(function (err) {
+                        if (err)
+                            return console.log(err);
+
+                        req.flash('success', 'Page added!');
+                        res.redirect('/admin/pages/edit-page/'+page.slug);
+                    });
+                
+                });
+
+
+            }
+        });
+    }
 
 });
 
