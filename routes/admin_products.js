0457b119189d1f69629889835d1dd454b3ef637d
Voja <Voja@DESKTOP-DCNG86L>
2017-08-08 10:52:32 +0200

    26 get add product

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -29,19 +29,23 @@ router.get('/', function (req, res) {
 });
 
 /*
- * GET add page
+ * GET add product
  */
-router.get('/add-page', function (req, res) {
+router.get('/add-product', function (req, res) {
 
     var title = "";
-    var slug = "";
-    var content = "";
+    var desc = "";
+    var price = "";
 
-    res.render('admin/add_page', {
-        title: title,
-        slug: slug,
-        content: content
+    Category.find(function (err, categories) {
+        res.render('admin/add_product', {
+            title: title,
+            desc: desc,
+            categories: categories,
+            price: price
+        });
     });
+
 
 });
 
