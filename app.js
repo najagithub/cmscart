c2f04ac38e7f806c7cb811b1f2da8edfcbade226
Voja <Voja@DESKTOP-DCNG86L>
2017-08-10 09:19:50 +0200

    34 front menu display

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var fileUpload = require('express-fileupload');
 mongoose.connect(config.database);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
     console.log('Connected to MongoDB');
 });
 
@@ -28,39 +28,51 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Set global errors variable
 app.locals.errors = null;
 
+// Get Page Model
+var Page = require('./models/page');
+
+// Get all pages to pass to header.ejs
+Page.find({}).sort({sorting: 1}).exec(function (err, pages) {
+    if (err) {
+        console.log(err);
+    } else {
+        app.locals.pages = pages;
+    }
+});
+
 // Express fileUpload middleware
 app.use(fileUpload());
 
 // Body Parser middleware
 // 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
 app.use(bodyParser.json());
 
 // Express Session middleware
 app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
 //  cookie: { secure: true }
 }));
 
 // Express Validator middleware
 app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+                , root = namespace.shift()
+                , formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
     },
     customValidators: {
         isImage: function (value, filename) {
@@ -84,8 +96,8 @@ app.use(expressValidator({
 // Express Messages middleware
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
+    res.locals.messages = require('express-messages')(req, res);
+    next();
 });
 
 // Set routes 
@@ -101,6 +113,6 @@ app.use('/', pages);
 
 // Start the server
 var port = 3000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Server started on port ' + port);
 });
