2de4d71ca7730b1590a30410c8e20477cf4e921b
Voja <Voja@DESKTOP-DCNG86L>
2017-08-11 12:52:23 +0200

    42 add to cart

--- routes/cart.js
+++ routes/cart.js
@@ -5,20 +5,51 @@ var router = express.Router();
 var Product = require('../models/product');
 
 /*
- * GET /
+ * GET add product to cart
  */
-router.get('/', function (req, res) {
-    
-    Page.findOne({slug: 'home'}, function (err, page) {
+router.get('/add/:product', function (req, res) {
+
+    var slug = req.params.product;
+
+    Product.findOne({slug: slug}, function (err, p) {
         if (err)
             console.log(err);
 
-        res.render('index', {
-            title: page.title,
-            content: page.content
-        });
+        if (typeof req.session.cart == "undefined") {
+            req.session.cart = [];
+            req.session.cart.push({
+                title: slug,
+                qty: 1,
+                price: parseFloat(p.price).toFixed(2),
+                image: '/product_images/' + p._id + '/' + p.image
+            });
+        } else {
+            var cart = req.session.cart;
+            var newItem = true;
+
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].title == slug) {
+                    cart[i].qty++;
+                    newItem = false;
+                    break;
+                }
+            }
+
+            if (newItem) {
+                cart.push({
+                    title: slug,
+                    qty: 1,
+                    price: parseFloat(p.price).toFixed(2),
+                    image: '/product_images/' + p._id + '/' + p.image
+                });
+            }
+        }
+        
+//        console.log(req.session.cart);
+        req.flash('success', 'Product added!');
+        res.redirect('back');
     });
-    
+
 });
 
 // Exports
